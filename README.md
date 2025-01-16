63ce7298249eae5e3d1aa45cceec4fa6126f927a
Koseki Nobuyuki <koseki.nobuyuki@gmail.com>
2025-01-16 19:41:12 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+echo "# vagrant" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/nkoseki/vagrant.git
+git push -u origin main
+
+---
+◆許可されているNWアドレスの変更する場合
+/etc/vbox/networks.conf
+* 172.16.0.0/24
+* 192.168.0.0/16 
+---
+◆virtual boxのデフォルトNWアドレスの割り当て
+192.168.56.0/24 
+
---
+++ wb01/api/index.js
@@ -0,0 +1,25 @@
+const port = 3000
+
+const express = require('express')
+const app = express()
+const data = require('./data.json')
+
+const getCurrentUser = ({ headers }) => {
+  return headers['mock-logged-in-as'] ||
+    headers['x-authenticated-userid']
+}
+
+app.get('/stepcounts', (req, res, next) => {
+  console.log(req.headers)
+  const user = getCurrentUser(req)
+  if (!user) {
+    res.status(401).send('Not authorized')
+    return
+  }
+  res.send(data[user] || [])
+})
+
+app.listen(port, () => {
+  console.log(`Server is listening on http://wb01:${port}`
+  )
+})
